eaab4412774ddc205fd48293bc5b04f3da83ea16
sarah rose c <68089778+sarahrosecooper@users.noreply.github.com>
2021-01-13 12:50:10 -0500

    just setting up empty faux api and state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
 import React from 'react';
 
+const toDo = [
+  { 
+    task: "",
+    id: "",
+    completed: false
+  },
+  { 
+    task: "",
+    id: "",
+    completed: false
+  },
+  {
+    task: "",
+    id: "",
+    completed: false
+  },
+  {
+    task: "",
+    id: "",
+    completed: false
+  },
+ {
+   task: "",
+   id: "",
+   completed: false
+ }
+]
+
 class App extends React.Component {
   // you will need a place to store your state in this component.
+  constructor(){
+    super();
+    this.state = {
+      toDo: toDo
+    }
+  }
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
   render() {
